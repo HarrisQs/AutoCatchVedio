3db23764d949c580162f1e5b3cab67621a5735b0
HarrisQs <harris32916@gmail.com>
2016-11-18 14:44:33 +0800

    Almost complete

--- AutoCatchVedio/AutoCatchVedio/Form1.Designer.cs
+++ AutoCatchVedio/AutoCatchVedio/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.GoalWeb = new System.Windows.Forms.WebBrowser();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -37,6 +38,16 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // GoalWeb
@@ -114,15 +125,116 @@
             this.label5.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label5.Location = new System.Drawing.Point(112, 195);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(54, 24);
+            this.label5.Size = new System.Drawing.Size(57, 24);
             this.label5.TabIndex = 8;
-            this.label5.Text = "4444";
+            this.label5.Text = "180 s";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.StartButton.Location = new System.Drawing.Point(12, 409);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(154, 65);
+            this.StartButton.TabIndex = 9;
+            this.StartButton.Text = "開始下載";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.StopButton.Location = new System.Drawing.Point(223, 409);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(154, 65);
+            this.StopButton.TabIndex = 10;
+            this.StopButton.Text = "暫停下載";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.Location = new System.Drawing.Point(8, 254);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 24);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "目前集數:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label7.Location = new System.Drawing.Point(112, 254);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(21, 24);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "0";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 80;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label8.Location = new System.Drawing.Point(112, 302);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(21, 24);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label9.Location = new System.Drawing.Point(8, 302);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(91, 24);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "開始時間:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label10.Location = new System.Drawing.Point(112, 346);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(21, 24);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "0";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label11.Location = new System.Drawing.Point(8, 346);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(91, 24);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "目前時間:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1308, 813);
+            this.ClientSize = new System.Drawing.Size(1276, 813);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox3);
@@ -150,6 +262,16 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
     }
 }
 
--- AutoCatchVedio/AutoCatchVedio/Form1.cs
+++ AutoCatchVedio/AutoCatchVedio/Form1.cs
@@ -12,9 +12,66 @@ namespace AutoCatchVedio
 {
     public partial class Form1 : Form
     {
+        private string URL;
+        private int RemainingTime = 5;
+        private int Start = 0;
         public Form1()
         {
             InitializeComponent();
+            timer1.Stop();//計時開始
+            timer2.Stop();//下載
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            URL = textBox1.Text;
+            Start = int.Parse(textBox2.Text);
+            label8.Text = DateTime.Now.ToString("yyyy-MM-dd HH:mm ss ");
+            timer1.Start();//計時開始
+            timer1.Interval = 1050;
+            timer2.Start();//下載
+            timer2.Interval = 2000;
+            StartButton.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label5.Text = (RemainingTime--).ToString()+" s";
+            label10.Text = DateTime.Now.ToString("yyyy-MM-dd HH:mm ss ");
+            if (RemainingTime == 0)
+                RemainingTime = 180;
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (Start <= int.Parse(textBox3.Text))
+            {
+                if (Start != int.Parse(textBox2.Text))
+                    URL = URL.Replace("pid-"+(Start - 1).ToString(), "pid-" + Start.ToString());
+                label7.Text = Start.ToString();
+                this.GoalWeb.Url = new Uri(URL);
+                Start++;
+            }
+            else
+            {
+                timer1.Stop();//計時開始
+                timer2.Stop();//下載
+            }
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            if (StopButton.Text == "暫停下載")
+            {
+                timer1.Stop();//計時開始
+                timer2.Stop();//下載
+                StopButton.Text = "重新開始";
+            }
+            else if (StopButton.Text == "重新開始")
+            {
+                timer1.Start();//計時開始
+                timer2.Start();//下載
+                StopButton.Text = "暫停下載";
+            }
         }
     }
 }
